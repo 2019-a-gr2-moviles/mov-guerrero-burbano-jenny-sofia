cf85c4793d9cda82050b07b0f9e36fd338c1643f
jsofii <sofi-0106@hotmail.com>
2019-04-30 17:06:07 -0500

    mostrar datos terminado

---
+++ Deber1/src/ffff.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ffff {
+    private JButton button1;
+    private JTable table1;
+
+
+    public ffff() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
